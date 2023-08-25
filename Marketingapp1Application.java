b75c0557ec81aba00edb867e3222940a90b5229a
Nitesh Mishra <83541924+nmishra484@users.noreply.github.com>
2023-08-25 10:11:09 +0530

    Add files via upload

---
+++ Marketingapp1Application.java
@@ -0,0 +1,15 @@
+package com.marketingapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.marketingapp1.util.EmailServiceImpl;
+
+@SpringBootApplication
+public class Marketingapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Marketingapp1Application.class, args);
+	}
+}
