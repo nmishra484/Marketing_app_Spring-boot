6226cbf9ede0e3084be09ac794ccd24dbdd201ed
Nitesh Mishra <83541924+nmishra484@users.noreply.github.com>
2023-08-25 10:10:47 +0530

    Add files via upload

---
+++ EmailServiceImpl.java
@@ -0,0 +1,22 @@
+package com.marketingapp1.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailServiceImpl implements EmailService {
+	
+	@Autowired 
+	private JavaMailSender javaMailSender;
+	@Override
+	public void sendEmail(String to, String sub, String message) {
+		// TODO Auto-generated method stub
+       SimpleMailMessage content = new SimpleMailMessage();
+       content.setTo(to);
+       content.setSubject(sub);
+       content.setText(message);
+       javaMailSender.send(content);
+	}
+}
